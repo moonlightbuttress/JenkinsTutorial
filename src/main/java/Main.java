acfea2ee39282996038f23dafb10f9d8bfcc9eb5
Braydn Tanner <tcrbraydn@gmail.com>
2020-02-23 19:31:55 -0700

    version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,50 +4,51 @@ import java.io.InputStreamReader;
 
 public class Main {
         public static void main(String[] args) throws IOException {
-                Calculator calc = new Calculator();
-                InputStreamReader is = new InputStreamReader(System.in);
-                BufferedReader br = new BufferedReader(is);
-                String input = "begin";
-                System.out.println("type exit to exit program");
-                while (!input.equals("exit")) {
-
-                        input = br.readLine();
-
-                        if ("add".equals(input)) {
-                                System.out.println("Enter 2 numbers to add");
-                                int numa = Integer.parseInt(br.readLine());
-                                int numb = Integer.parseInt(br.readLine());
-                                System.out.println(calc.add(numa, numb));
-                        } else if ("subtract".equals(input)) {
-                                System.out.println("Enter 2 numbers to subtract");
-                                int numa = Integer.parseInt(br.readLine());
-                                int numb = Integer.parseInt(br.readLine());
-                                System.out.println(calc.subtract(numa, numb));
-                        } else if ("multiply".equals(input)) {
-                                System.out.println("Enter 2 numbers to multiply");
-                                int numa = Integer.parseInt(br.readLine());
-                                int numb = Integer.parseInt(br.readLine());
-                                System.out.println(calc.multiply(numa, numb));
-                        } else if ("divide".equals(input)) {
-                                System.out.println("Enter 2 numbers to divide");
-                                int numa = Integer.parseInt(br.readLine());
-                                int numb = Integer.parseInt(br.readLine());
-                                System.out.println(calc.divide(numa, numb));
-                        } else if ("fibonacci".equals(input)) {
-                                System.out.println("Enter Nth fibonnaci number");
-                                int numa = Integer.parseInt(br.readLine());
-                                System.out.println(calc.fibonacciNumberFinder(numa));
-                        } else if ("binary".equals(input)) {
-                                System.out.println("Enter a number to convert to Binary");
-                                int numa = Integer.parseInt(br.readLine());
-                                System.out.println(calc.intToBinaryNumber(numa));
-                        } else if ("uniqueID".equals(input)) {
-                                System.out.println("Enter something to make a unique ID");
-                                String numa = br.readLine();
-                                System.out.println(calc.createUniqueID(numa));
-                        } else {
-                                System.out.println("invalid input");
-                        }
-                }
+//                Calculator calc = new Calculator();
+//                InputStreamReader is = new InputStreamReader(System.in);
+//                BufferedReader br = new BufferedReader(is);
+//                String input = "begin";
+//                System.out.println("type exit to exit program");
+//                while (!input.equals("exit")) {
+//
+//                        input = br.readLine();
+//
+//                        if ("add".equals(input)) {
+//                                System.out.println("Enter 2 numbers to add");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                int numb = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.add(numa, numb));
+//                        } else if ("subtract".equals(input)) {
+//                                System.out.println("Enter 2 numbers to subtract");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                int numb = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.subtract(numa, numb));
+//                        } else if ("multiply".equals(input)) {
+//                                System.out.println("Enter 2 numbers to multiply");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                int numb = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.multiply(numa, numb));
+//                        } else if ("divide".equals(input)) {
+//                                System.out.println("Enter 2 numbers to divide");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                int numb = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.divide(numa, numb));
+//                        } else if ("fibonacci".equals(input)) {
+//                                System.out.println("Enter Nth fibonnaci number");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.fibonacciNumberFinder(numa));
+//                        } else if ("binary".equals(input)) {
+//                                System.out.println("Enter a number to convert to Binary");
+//                                int numa = Integer.parseInt(br.readLine());
+//                                System.out.println(calc.intToBinaryNumber(numa));
+//                        } else if ("uniqueID".equals(input)) {
+//                                System.out.println("Enter something to make a unique ID");
+//                                String numa = br.readLine();
+//                                System.out.println(calc.createUniqueID(numa));
+//                        } else {
+//                                System.out.println("invalid input");
+//                        }
+//                }
+            System.out.println("add\n1\n1\n2\nsubtract\n2\n1\n1\nmultiply\n1\n4\n4\ndivide\n5\n2\n10\nfibonacci\n4\n3\nbinary\n8\n1000\n");
         }
 }
